874e51924a89f39de8d335a383493f02efec9839
HR.KIM <jamesol@paran.com>
2019-09-17 17:58:19 +0900

    init

---
+++ eHR0503_WEB05/src/main/java/kr/co/ehr/user/service/UserService.java
@@ -0,0 +1,18 @@
+package kr.co.ehr.user.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface UserService {
+
+	public String get_excelDown(Search vo,String ext);
+	public int update(User user);
+	public List<User> retrieve(Search vo);
+	public int deleteUser(User user);
+	public User get(String id);
+	
+	// 최초 사용자 베이직 레벨
+	public int add(User user);
+
+	public void tx_upgradeLevels() throws SQLException;
+}
